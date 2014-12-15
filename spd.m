840652c0e66b1e2435db03f445f6dfe8e611e689
zhaojunbo <zhaojunbo1992chasing@gmail.com>
2014-12-14 22:38:23 -0500

    Add steepest descent

--- spd.m
+++ spd.m
@@ -1,21 +1,20 @@
-% This script implements Newton's method
+% This script implements Steepest descent
 
-function x = Newton(init=[0,0,0])
-% H - Hessian
+function x = spd(init=[0,0,0])
 % F - Function
 % J - Gradient
+% 
     A = [1 1 0; 1 2 1; 0 1 2];
     b = [-2; 0; 0];
     F = @(x) 1/ 2 * x'*A*x -b'*x;
     J = @(x) A*x-b;
-    H = A;
 
     x = reshape(init, [3,1]);
     k = 0;
     while(1)
-        p = -H \ J(x);
-        x = x + p;
-        %disp('x: '), disp(x)
+        Falp = @(alp) F(x-alp*J(x));
+        alpha = fminsearch(Falp, 1);
+        x = x - alpha*J(x);
         if (abs(J(x)) < 1e-5)
             break
         end
